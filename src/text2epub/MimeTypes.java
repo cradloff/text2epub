1ded23a66a3c22c1d4ee8e706148d6fea53edd50
Claus Radloff <Claus.Radloff@web.de>
2017-03-03 17:24:57 +0100

    bessere Unterstützung von Mime-Types

--- src/text2epub/MimeTypes.java
+++ src/text2epub/MimeTypes.java
@@ -1,5 +1,8 @@
 package text2epub;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /** Hilfsklasse zur Bestimmung von Mime-Types */
 public class MimeTypes {
 	public static final String MIME_TYPE_CSS = "text/css";
@@ -13,6 +16,21 @@ public class MimeTypes {
 		{ "image/png",	".png" }, // PNG-Dateien
 		{ MIME_TYPE_SVG, ".svg" } // SVG-Grafiken
 	};
+	/** Mime-Type je Dateiendung */
+	private static final Map<String, String> MIME_TYPES;
+	static {
+		Map<String, String> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+		map.put(".css", MIME_TYPE_CSS);
+		map.put(".html", MIMETYPE_XHTML);
+		map.put(".xhtml", MIMETYPE_XHTML);
+		for (String[] values : MIME_TYPES_IMAGE) {
+			for (int i = 1; i < values.length; i++) {
+				map.put(values[i], values[0]);
+			}
+		}
+
+		MIME_TYPES = map;
+	}
 
 	/**
 	 * Ermittelt den Mime-Type zum angegebenen Datei-Namen für ein Bild.
@@ -20,15 +38,8 @@ public class MimeTypes {
 	 * @return Mime-Type
 	 */
 	public static String getMimeType(String filename) {
-		String mimeType = null;
 		String suffix = filename.substring(filename.lastIndexOf('.'));
-		for (String[] s : MIME_TYPES_IMAGE) {
-			for (int i = 1; i < s.length; i++) {
-				if (suffix.equalsIgnoreCase(s[i])) {
-					mimeType = s[0];
-				}
-			}
-		}
+		String mimeType = MIME_TYPES.get(suffix);
 
 		if (mimeType == null) {
 			throw new IllegalArgumentException("unknown mime-type for suffix " + suffix);
